04e0f80d01292caeb2bdbebf9a9949c426966826
Leslie Montgomery <montgomery.leslie@gmail.com>
2026-03-05 18:55:34 -0500

    feat: add initial hello.route.ts for tsoa testing

---
+++ src/hello.route.ts
@@ -0,0 +1,9 @@
+import { Route, Get, Path } from 'tsoa';
+
+@Route('hello')
+export class HelloRoute {
+  @Get('{name}')
+  public async sayHello(@Path() name: string): Promise<string> {
+    return `Hello ${name}`;
+  }
+}
