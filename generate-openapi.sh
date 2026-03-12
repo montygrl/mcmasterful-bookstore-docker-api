7baa8966b5512cb1184d5631d70bc70657f8b88a
Leslie Montgomery <montgomery.leslie@gmail.com>
2026-03-12 22:54:09 +0000

    fix: add --skip-validate-spec and patch KoaRouter type in routes.ts

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 npx tsoa spec-and-routes
-npx @openapitools/openapi-generator-cli generate -i ./build/swagger.json -o ./client -g typescript-fetch
+sed -i "s/import type \* as KoaRouter from '@koa\/router'/import Router from '@koa\/router'/" build/routes.ts
+sed -i "s/router: KoaRouter/router: Router/" build/routes.ts
+npx @openapitools/openapi-generator-cli generate -i ./build/swagger.json -o ./client -g typescript-fetch --skip-validate-spec
